940881aab58539296f1a5811624a60be636e2a5b
Fer_ <fg.roglia@gmail.com>
2024-12-04 17:45:31 +0100

    Ejercicio_5_Nivel_1_Finalizado

---
+++ src/nivel_1/ejercicio5/Main.java
@@ -0,0 +1,11 @@
+package nivel_1.ejercicio5;
+
+import static nivel_1.ejercicio5.SerializableMethod.applySerializableMethod;
+
+public class Main {
+    public static void main(String[] args) {
+
+        applySerializableMethod();
+
+    }
+}
---
+++ src/nivel_1/ejercicio5/SerializableMethod.java
@@ -0,0 +1,44 @@
+package nivel_1.ejercicio5;
+
+import java.io.*;
+
+public class SerializableMethod {
+
+
+    public static void applySerializableMethod() {
+
+        Person person = new Person("Rolo", 40);
+
+        serializePerson(person);
+
+        deserializePerson();
+
+    }
+
+    public static void serializePerson(Person person) {
+
+        try {
+            String filePath = "person.ser";
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath));
+            objectOutputStream.writeObject(person);
+            System.out.println("Serialized object: " + person);
+            objectOutputStream.close();
+
+        } catch (IOException e) {
+            System.out.println("Error durante la serialización: " + e.getMessage());
+        }
+    }
+
+    public static void deserializePerson() {
+
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("person.ser"));
+            Person person = (Person) objectInputStream.readObject();
+            System.out.println("Deserialized object: " + person);
+
+        } catch (IOException | ClassNotFoundException e) {
+
+            System.out.println("Error deserializing Person object: " + e.getMessage());
+        }
+    }
+}
